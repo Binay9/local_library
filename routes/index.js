97475821d2ae2be16536f283ae247a1147138fa9
Binay9 <soundofpeace.bd@gmail.com>
2020-11-05 01:47:40 +0000

    added controllers and routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express modified' });
+
+router.get('/', function(req, res){
+  res.redirect('/catalog');
 });
+
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express modified' });
+// });
+
+
 
 module.exports = router;
